c989cf18cdbf93b1edada052d1e8a2fa581ebc71
kateusacova <kateusacova@gmail.com>
2022-09-27 17:18:40 +0100

    Wrote and tested create method and refactored  album repo

--- lib/album_repository.rb
+++ lib/album_repository.rb
@@ -7,13 +7,7 @@ class AlbumRepository
     albums = []
 
     result_set.each { |record| 
-      album = Album.new
-      album.id = record["id"]
-      album.title = record["title"]
-      album.release_year = record["release_year"]
-      album.artist_id = record["artist_id"]
-
-      albums << album
+      albums << convert_to_object(record)
     }
 
     albums
@@ -26,12 +20,25 @@ class AlbumRepository
 
     record = result[0]
 
-    album = Album.new
-    album.id = record["id"]
-    album.title = record["title"]
-    album.release_year = record["release_year"]
-    album.artist_id = record["artist_id"]
+    convert_to_object(record)
+  end
 
-    album
+  def create(album)
+    query = "INSERT INTO albums (title, release_year, artist_id) VALUES ($1, $2, $3);"
+    params = [album.title, album.release_year, album.artist_id]
+    DatabaseConnection.exec_params(query, params)
+  end
+
+  private
+
+  def convert_to_object(record)
+      album = Album.new
+
+      album.id = record["id"]
+      album.title = record["title"]
+      album.release_year = record["release_year"]
+      album.artist_id = record["artist_id"]
+      
+      album
   end
 end
--- spec/album_repository_spec.rb
+++ spec/album_repository_spec.rb
@@ -25,4 +25,21 @@ RSpec.describe AlbumRepository do
     expect(album.id).to eq "1"
     expect(album.title).to eq 'Doolittle'
   end
+
+  it "create a new Album object with specified parameters" do
+    repository = AlbumRepository.new
+    album = Album.new
+    album.title = 'Trompe le Monde'
+    album.release_year = 1991
+    album.artist_id = 1
+    repository.create(album)
+    all_albums = repository.all
+    expect(all_albums).to include(
+      have_attributes(
+        title: album.title,
+        release_year: "1991",
+        artist_id: "1"
+      )
+    )
+  end
 end
