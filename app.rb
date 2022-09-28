b39798c557ec3cc77b6922a5973ead90c70cac5a
kateusacova <kateusacova@gmail.com>
2022-09-28 18:22:03 +0100

    Developed and tested Application class interface

--- app.rb
+++ app.rb
@@ -1,11 +1,49 @@
-require_relative 'lib/database_connection'
-require_relative 'lib/artist_repository'
-require_relative 'lib/album_repository'
+require_relative './lib/album_repository'
+require_relative './lib/artist_repository'
+require_relative './lib/database_connection'
 
-DatabaseConnection.connect('music_library')
+class Application
 
-album_repository = AlbumRepository.new
-artist_repository = ArtistRepository.new
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
 
-# p artist_repository.find(1)
-p album_repository.find(1)
+  def run
+    @io.puts "\n"
+    @io.puts "Welcome to the music library manager!"
+    @io.puts "\n"
+    @io.puts "What would you like to do?"
+    @io.puts "1 - List all albums"
+    @io.puts "2 - List all artists"
+    @io.puts "\n"
+    @io.puts "Enter your choice:"
+
+    selection = @io.gets.chomp
+
+    if selection == "1"
+      @io.puts "Here is the list of albums:"
+      @album_repository.all.each { |record|
+        @io.puts "* #{record.id} - #{record.title}"
+      }
+    else
+      @io.puts "Here is the list of artists:"
+      @artist_repository.all.each { |record|
+        @io.puts "* #{record.id} - #{record.name}"
+      } 
+    end
+  end
+end
+
+
+if __FILE__ == $0
+  app = Application.new(
+    'music_library',
+    Kernel,
+    AlbumRepository.new,
+    ArtistRepository.new
+  )
+  app.run
+end
---
+++ spec/app_spec.rb
@@ -0,0 +1,70 @@
+require_relative "../app.rb"
+require_relative "../lib/album_repository.rb"
+require_relative "../lib/artist_repository.rb"
+require 'dotenv/load'
+
+def reset_albums_table
+  seed_sql = File.read('spec/seeds_albums.sql')
+  connection = PG.connect({ host: ENV['HOST'], dbname: 'music_library_test', user: 'postgres', password: ENV['PASSWORD'] })
+  connection.exec(seed_sql)
+end
+
+def reset_artists_table
+  seed_sql = File.read('spec/seeds_artists.sql')
+  connection = PG.connect({ host: ENV['HOST'], dbname: 'music_library_test', user: 'postgres', password: ENV['PASSWORD'] })
+  connection.exec(seed_sql)
+end
+
+RSpec.describe Application do
+  before(:each) do 
+    reset_albums_table
+  end
+
+  it "Runs the program" do
+    io = double :io
+    album_repository = AlbumRepository.new
+    artist_repository = double :artist_repository
+    app = Application.new("music_library_test", io, album_repository, artist_repository)
+
+    expect(io).to receive(:puts).with("\n")
+    expect(io).to receive(:puts).with("Welcome to the music library manager!")
+    expect(io).to receive(:puts).with("\n")
+    expect(io).to receive(:puts).with("What would you like to do?")
+    expect(io).to receive(:puts).with("1 - List all albums")
+    expect(io).to receive(:puts).with("2 - List all artists")
+    expect(io).to receive(:puts).with("\n")
+    expect(io).to receive(:puts).with("Enter your choice:")
+    
+    expect(io).to receive(:gets).and_return("1")
+    
+    expect(io).to receive(:puts).with("Here is the list of albums:")
+    expect(io).to receive(:puts).with("* 1 - Doolittle")
+    expect(io).to receive(:puts).with("* 2 - Surfer Rosa")
+    
+    app.run
+  end
+
+  it "Runs the program" do
+    io = double :io
+    album_repository = double :album_repository
+    artist_repository = ArtistRepository.new
+    app = Application.new("music_library_test", io, album_repository, artist_repository)
+
+    expect(io).to receive(:puts).with("\n")
+    expect(io).to receive(:puts).with("Welcome to the music library manager!")
+    expect(io).to receive(:puts).with("\n")
+    expect(io).to receive(:puts).with("What would you like to do?")
+    expect(io).to receive(:puts).with("1 - List all albums")
+    expect(io).to receive(:puts).with("2 - List all artists")
+    expect(io).to receive(:puts).with("\n")
+    expect(io).to receive(:puts).with("Enter your choice:")
+    
+    expect(io).to receive(:gets).and_return("2")
+    
+    expect(io).to receive(:puts).with("Here is the list of artists:")
+    expect(io).to receive(:puts).with("* 1 - Pixies")
+    expect(io).to receive(:puts).with("* 2 - ABBA")
+    
+    app.run
+  end
+end
